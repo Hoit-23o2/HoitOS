1b56ed407b0ad7711e3b70e07c77d6645755dc9d
Yanqi Pan <deadpoolmine@qq.com>
2021-04-25 10:18:23 +0800

    2021-04-25 完成merge

--- SylixOS/Workspace/SylixBSP/SylixOS/extfs/hoitFs/hoitFsTreeUtil.h
+++ SylixOS/Workspace/SylixBSP/SylixOS/extfs/hoitFs/hoitFsTreeUtil.h
@@ -1,22 +1,22 @@
 /*********************************************************************************************************
 **
-**                                    涓浗杞欢寮�婧愮粍缁�
+**                                    �?���?���?源组�?
 **
-**                                   宓屽叆寮忓疄鏃舵搷浣滅郴缁�
+**                                   嵌入式实时操作系�?
 **
 **                                       SylixOS(TM)
 **
 **                               Copyright  All Rights Reserved
 **
-**--------------鏂囦欢淇℃伅--------------------------------------------------------------------------------
+**--------------文件信息--------------------------------------------------------------------------------
 **
-** 鏂�   浠�   鍚�: hoitFsTreeUtil.h
+** �?   �?   �?: hoitFsTreeUtil.h
 **
-** 鍒�   寤�   浜�: Pan yanqi (娼樺欢楹�)
+** �?   �?   �?: Pan yanqi (潘延�?)
 **
-** 鏂囦欢鍒涘缓鏃ユ湡: 2021 骞� 03 鏈� 28 鏃�
+** 文件创建日期: 2021 �? 03 �? 28 �?
 **
-** 鎻�        杩�: JFFS2-Like 绾㈤粦鏍戞暟鎹粨鏋�
+** �?        �?: JFFS2-Like 红黑树数�?���?
 *********************************************************************************************************/
 #define  __SYLIXOS_STDIO
 #define  __SYLIXOS_KERNEL
@@ -30,7 +30,7 @@
 #define RB_BLACK    1
 #define RB_RED      0
 /*********************************************************************************************************
-  绾㈤粦鏍戣妭鐐瑰畾涔�
+  红黑树节点定�?
 *********************************************************************************************************/
 typedef struct hoit_rb_node
 {
@@ -49,22 +49,22 @@ static inline PHOIT_RB_NODE newHoitRbNode(INT32 iKey){
     return pRbn;
 }
 /*********************************************************************************************************
-  绾㈤粦鏍戝畾涔�
+  红黑树定�?
 *********************************************************************************************************/
 typedef struct hoit_rb_tree
 {
-    PHOIT_RB_NODE pRbnGuard;            /* 鍝ㄥ叺 */
-    PHOIT_RB_NODE pRbnRoot;             /* 鏍硅妭鐐� */
+    PHOIT_RB_NODE pRbnGuard;            /* 哨兵 */
+    PHOIT_RB_NODE pRbnRoot;             /* 根节�? */
 } HOIT_RB_TREE;
 
 typedef HOIT_RB_TREE * PHOIT_RB_TREE;
 
 
 
-PHOIT_RB_TREE     hoitRbInitTree(VOID);                                          /* 鍒濆鍖朢B鏍� */
-PHOIT_RB_NODE     hoitRbInsertNode(PHOIT_RB_TREE pRbTree, PHOIT_RB_NODE pRbn);   /* 鎻掑叆涓�涓妭鐐� */
-PHOIT_RB_NODE     hoitRbSearchNode(PHOIT_RB_TREE pRbTree, INT32 iKey);           /* 鏌ユ壘涓�涓妭鐐� */
-BOOL              hoitRbDeleteNode(PHOIT_RB_TREE pRbTree, PHOIT_RB_NODE pRbn);   /* 鍒犻櫎涓�涓妭鐐� */
+PHOIT_RB_TREE     hoitRbInitTree(VOID);                                          /* 初�?化RB�? */
+PHOIT_RB_NODE     hoitRbInsertNode(PHOIT_RB_TREE pRbTree, PHOIT_RB_NODE pRbn);   /* 插入�?�?���? */
+PHOIT_RB_NODE     hoitRbSearchNode(PHOIT_RB_TREE pRbTree, INT32 iKey);           /* 查找�?�?���? */
+BOOL              hoitRbDeleteNode(PHOIT_RB_TREE pRbTree, PHOIT_RB_NODE pRbn);   /* 删除�?�?���? */
 
 #ifdef RB_TEST
 VOID hoitRbTreeTest();
