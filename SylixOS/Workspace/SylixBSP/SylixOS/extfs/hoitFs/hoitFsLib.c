ad21b00a469d0cbd933bfdff33efa869abb0776e
Deadpoolmine <48178553+Deadpoolmine@users.noreply.github.com>
2021-08-08 14:13:16 +0800

    2021-08-08 修复small write 与seqential write测试问题，目前close有问题

--- SylixOS/Workspace/SylixBSP/SylixOS/extfs/hoitFs/hoitFsLib.c
+++ SylixOS/Workspace/SylixBSP/SylixOS/extfs/hoitFs/hoitFsLib.c
@@ -1734,7 +1734,7 @@ INT  __hoit_stat(PHOIT_INODE_INFO pInodeInfo, PHOIT_VOLUME  pfs, struct stat* ps
 INT  __hoit_statfs(PHOIT_VOLUME  pfs, struct statfs* pstatfs) {
     pstatfs->f_type = TMPFS_MAGIC;  //ĞèÒªĞŞ¸Ä
     pstatfs->f_bsize = pfs->HOITFS_cacheHdr->HOITCACHE_blockSize;
-    pstatfs->f_blocks = 27;
+    pstatfs->f_blocks = 28;
     pstatfs->f_bfree = 0;
     pstatfs->f_bavail = 1;
 
